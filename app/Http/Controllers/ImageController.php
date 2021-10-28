4e103cd7f7dc7aab16c39d9149e95b11c75a340e
Vahid Molaei Bejarbaneh <91679087+vmolaei@users.noreply.github.com>
2021-10-28 16:23:47 +0330

    Initial commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+
+class ImageController extends Controller
+{
+    public function uoload(){
+        return view("upload");
+    }
+    public function save_image(Request $request){
+        if($request->hasFile('img_src')){
+            $image = $request->file('img_src');
+            $reimage = time().'.'.$image->getClientOriginalExtension();
+            $dest = public_path('imgs');
+            $image->move($dest,$reimage);
+            //save data
+            $image = new Image();
+            $image->img_alt = $request->img_alt;
+            $image->img_src = $reimage;
+            $image->save();
+            return redirect("image/upload")->with("success","image has been uploaded");
+
+
+        }else{
+            return redirect("image/upload")->with("msg","please choose file");
+        }
+    }
+}
---
+++ resources/views/upload.blade.php
@@ -0,0 +1,44 @@
+
+<html lang="en">
+<head>
+
+    <title>upload image</title>
+    <meta charset="utf-8">
+<!--<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    <script type="text/javascript" src="{{asset('js/jquery')}}"></script>-->
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/jquery.min.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h3>Create Form</h3>
+    <p class="ajax-res"></p>
+    @if(Session::has('success'))
+        <p class="text-success">{{session('success')}}</p>
+    @endif
+
+
+
+    @if(Session::has('msg'))
+        <p class="text-danger">{{session('msg')}}</p>
+    @endif
+    <form method="post" action="{{url('image/upload')}}" enctype="multipart/form-data" >
+        @csrf
+        <table class="table table-bordered">
+            <th>Alt Text</th>
+            <td><input type="text" name="img_alt" class="form-control"/> </td>
+            <tr>
+                <th>Image</th>
+                <td><input type="file" name="img_src" class="form-control"/></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" class="btn btn-success save-data" value="upload"></td>
+            </tr>
+        </table>
+
+    </form>
+</div>
+</body>
+</html>
